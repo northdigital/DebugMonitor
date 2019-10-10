58c2c13270e3b2bb9c1b3b0c770b768883f3ab68
panagiotis <piperopoulos@logismos.gr>
2019-10-10 11:29:17 +0300

    Update 03-grant.sql

--- scripts/03-grant.sql
+++ scripts/03-grant.sql
@@ -1,1 +1,3 @@
 grant execute on debugmonitor.utils to startouch with grant option;
+grant execute on debugmonitor.utils to staroverview with grant option;
+grant execute on debugmonitor.utils to casinocrm with grant option;
