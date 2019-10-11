511539d2cbb0372a0b636571b641df5736c04bbd
northdigital <10321991+northdigital@users.noreply.github.com>
2019-10-11 13:13:42 +0300

    Update 03-grant.sql

--- scripts/03-grant.sql
+++ scripts/03-grant.sql
@@ -1,3 +1,4 @@
 grant execute on debugmonitor.utils to startouch with grant option;
 grant execute on debugmonitor.utils to staroverview with grant option;
 grant execute on debugmonitor.utils to casinocrm with grant option;
+grant execute on debugmonitor.utils to logismos with grant option;
