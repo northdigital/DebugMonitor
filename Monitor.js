98159448ec45cc28619737d174c01f6520120555
northdigital <10321991+northdigital@users.noreply.github.com>
2019-11-22 15:30:05 +0200

    commit

--- Monitor.js
+++ Monitor.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var url = require('url');
+var moment = require('moment');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
-  res.end();
+  res.statusCode = 200;
+
+  var strDate = moment(new Date()).format('MM/DD/YYYY H:mm:ss');
+
+  res.end(`<html><html><h1>${strDate}</h1></body></html>`);
 
   /** @namespace q.msg **/
   if(q.msg)
