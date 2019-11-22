ef1758123093c7f9a63763458e0303ce9974366f
northdigital <10321991+northdigital@users.noreply.github.com>
2019-11-22 15:48:12 +0200

    Update Monitor.js

--- Monitor.js
+++ Monitor.js
@@ -1,20 +1,26 @@
 var http = require('http');
 var url = require('url');
 var moment = require('moment');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
-  res.statusCode = 200;
 
   var strDate = moment(new Date()).format('MM/DD/YYYY H:mm:ss');
-
+  res.statusCode = 200;
   res.end(`<html><html><h1>${strDate}</h1></body></html>`);
 
-  /** @namespace q.msg **/
   if(q.msg)
-    if(q.msg.toUpperCase() === '@CLEAR@')
+    if(q.msg.toUpperCase() === '@CLEAR@') {
       console.clear();
-    else
+    } else {
       console.log(q.msg);
+      if(q.file) {
+        fs.appendFile(q.file, `${q.msg}\n`, function (err) { 
+          if(err)         
+            console.log(err);
+        }); 
+      }
+    }
 }).listen(4321);
