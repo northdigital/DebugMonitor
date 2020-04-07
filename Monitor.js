a20a2ad148776b1ab6f2a3cf79337398f1277115
admin <admin@centos07.hercules>
2020-04-07 22:19:19 +0300

    find my IP address

--- Monitor.js
+++ Monitor.js
@@ -2,6 +2,24 @@ var http = require('http');
 var url = require('url');
 var moment = require('moment');
 var fs = require('fs');
+const port = 4321;
+
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+
+  return '0.0.0.0';
+}
+
+console.log(`listening at ${getIPAddress()}:${port}`);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
