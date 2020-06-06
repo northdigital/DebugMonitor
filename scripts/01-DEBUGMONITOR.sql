d7adb88af69fadfe7c575a7da1de01c766d02db8
northdigital <10321991+northdigital@users.noreply.github.com>
2020-06-06 13:00:31 +0300

    commit

--- scripts/01-DEBUGMONITOR.sql
+++ scripts/01-DEBUGMONITOR.sql
@@ -5,30 +5,30 @@ create user DEBUGMONITOR identified externally;
 grant execute on utl_http to DEBUGMONITOR with grant option;
 
 /*
-BEGIN DBMS_NETWORK_ACL_ADMIN.DROP_ACL(acl => '/sys/acls/DEBUGMONITOR-HERCULES.xml'); END;
+BEGIN DBMS_NETWORK_ACL_ADMIN.DROP_ACL(acl => '/sys/acls/DEBUGMONITOR.xml'); END;
 */
 
 
 BEGIN
-  DBMS_NETWORK_ACL_ADMIN.create_acl(acl => 'DEBUGMONITOR-HERCULES.xml', 
-                                    description => 'ACL for GDPR',
+  DBMS_NETWORK_ACL_ADMIN.create_acl(acl => 'DEBUGMONITOR.xml', 
+                                    description => 'ACL',
                                     PRINCIPAL => 'DEBUGMONITOR', 
                                     is_grant => TRUE, 
                                     privilege => 'connect',
                                     start_date => SYSTIMESTAMP, 
                                     end_date => NULL);
   
-  DBMS_NETWORK_ACL_ADMIN.ADD_PRIVILEGE(acl => 'DEBUGMONITOR-HERCULES.xml', 
+  DBMS_NETWORK_ACL_ADMIN.ADD_PRIVILEGE(acl => 'DEBUGMONITOR.xml', 
                                        principal => 'DEBUGMONITOR', 
                                        is_grant => true,
                                        privilege => 'connect');
 
-  DBMS_NETWORK_ACL_ADMIN.ADD_PRIVILEGE(acl => 'DEBUGMONITOR-HERCULES.xml', 
+  DBMS_NETWORK_ACL_ADMIN.ADD_PRIVILEGE(acl => 'DEBUGMONITOR.xml', 
                                        principal => 'DEBUGMONITOR', 
                                        is_grant => true,
                                        privilege => 'resolve');
 
-  dbms_network_acl_admin.assign_acl(acl => 'DEBUGMONITOR-HERCULES.xml', 
+  dbms_network_acl_admin.assign_acl(acl => 'DEBUGMONITOR.xml', 
                                     host => /*node host*/ '192.168.1.202', 
                                     lower_port => 4321, 
                                     upper_port => null);
